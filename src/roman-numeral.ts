464b7b0af0eb1e5cabdf1a0b4fdceedf895b804c
Suzalen <baptiste.perri.a10@gmail.com>
2025-05-23 11:27:58 +0200

    Ajout de divers tests

--- src/roman-numeral.ts
+++ src/roman-numeral.ts
@@ -2,16 +2,27 @@ export class RomanNumeral {
   private readonly values: { [key: string]: number } = {
     I: 1,
     V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
   };
 
   of(value: string): number {
-    let result = 0;
-    for (const char of value) {
-      if (!(char in this.values)) {
-        throw new Error('Not implemented');
-      }
-      result += this.values[char];
+  let result = 0;
+  for (let i = 0; i < value.length; i++) {
+    if (!(value[i] in this.values)) {
+      throw new Error('Not implemented');
     }
-    return result;
+    const current = this.values[value[i]];
+    const next = i + 1 < value.length && (value[i + 1] in this.values) ? this.values[value[i + 1]] : 0;
+    if (next > current) {
+      result -= current;
+    } else {
+      result += current;
+    }
   }
+  return result;
+}
 }
--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,5 +1,4 @@
 // Write your test here
-import { test } from '../../../../.cache/deno/npm/registry.npmjs.org/@vitest/runner/1.6.1/dist/index.d.ts';
 import { RomanNumeral } from './roman-numeral.ts';
 import { describe, beforeEach, it, expect } from 'vitest';
 
@@ -21,4 +20,51 @@ describe('RomanNumeral', () => {
   it('should return 2 for "II"', () => { 
     expect(romanNumeral.of('II')).toBe(2); });
 
+  it('sould return 4 for "IV"', () => {
+    expect(romanNumeral.of('IV')).toBe(4);
+  });
+
+  it('should return 3 for "III"', () => {
+    expect(romanNumeral.of('III')).toBe(3);
+  });
+
+  it('should return 9 for "IX"', () => {
+    expect(romanNumeral.of('IX')).toBe(9);
+  });
+
+  it('should return 10 for "X"', () => {
+    expect(romanNumeral.of('X')).toBe(10);
+  });
+
+  it('should return 10 for VV', () => {
+    expect(romanNumeral.of('VV')).toBe(10);
+  });
+
+  it('should return 50 for "L"', () => {
+    expect(romanNumeral.of('L')).toBe(50);
+  });
+
+  it('should return 100 for "C"', () => {
+    expect(romanNumeral.of('C')).toBe(100);
+  });
+
+  it('should return 500 for "D"', () => {
+    expect(romanNumeral.of('D')).toBe(500);
+  });
+
+  it('should return 1000 for "M"', () => {
+    expect(romanNumeral.of('M')).toBe(1000);
+  });
+
+  it('should return 490 for "CDXC"', () => {
+    expect(romanNumeral.of('CDXC')).toBe(490);
+  });
+
+  it('should return 1049 for "MXLIX"', () => {
+    expect(romanNumeral.of('MXLIX')).toBe(1049);
+  });
+
+  it('shoul return 2025 for "MMXXV"', () => {
+    expect(romanNumeral.of('MMXXV')).toBe(2025);
+  });
 });
