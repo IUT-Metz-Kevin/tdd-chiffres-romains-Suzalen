e7182de60a076f85e37afafb8c5b73e4a17ade5b
Suzalen <baptiste.perri.a10@gmail.com>
2025-05-23 11:08:01 +0200

    Changement classe Roman pour accepter V

--- src/roman-numeral.ts
+++ src/roman-numeral.ts
@@ -1,6 +1,13 @@
 export class RomanNumeral {
+  private readonly values: { [key: string]: number } = {
+    I: 1,
+    V: 5,
+  };
+
   of(value: string): number {
-    if (value === 'I') return 1;
+  if (!(value in this.values)) {
     throw new Error('Not implemented');
   }
+  return this.values[value];
+}
 }
