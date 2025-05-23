10c2a3f07152dc622fa99161863b7ce643176c3f
Suzalen <baptiste.perri.a10@gmail.com>
2025-05-23 10:55:28 +0200

    First Test True

--- src/roman-numeral.ts
+++ src/roman-numeral.ts
@@ -1,4 +1,4 @@
-export class Roman {
+export class RomanNumeral {
   of(value: string): number {
     if (value === 'I') return 1;
     throw new Error('Not implemented');
--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,15 @@
 // Write your test here
+import { RomanNumeral } from './roman-numeral.ts';
+import { describe, beforeEach, it, expect } from 'vitest';
+
+describe('RomanNumeral', () => {
+  let romanNumeral: RomanNumeral;
+
+  beforeEach(() => {
+    romanNumeral = new RomanNumeral();
+  });
+
+  it('should return 1 for "I"', () => {
+    expect(romanNumeral.of('I')).toBe(1);
+  });
+});
